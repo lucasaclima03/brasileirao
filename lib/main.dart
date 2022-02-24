88a962d8e9869ccd931fdd93cde7158e6331e1d0
Lucas Almeida <lucasaclima03@gmail.com>
2022-02-24 13:22:11 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:brasileirao/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Brasileirão',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
