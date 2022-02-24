081bf583a43f484e08cbedc84c7822b56c208f40
Lucas Almeida <lucasaclima03@gmail.com>
2022-02-24 13:43:35 -0300

    criando home page e controller

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,32 @@
+import 'package:brasileirao/pages/home_controller.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  var controller = HomeController();
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('texto'),
+      appBar: AppBar(
+        title: Center(
+          child: Text('Brasileirão'),
+        ),
+      ),
+      body: ListView.separated(
+        itemBuilder: (context, int i) {
+          final tabela = controller.tabela;
+          return ListTile(
+            leading: Image.network(
+              tabela[i].brasao,
+            ),
+            title: Text(tabela[i].nome),
+            trailing: Text(tabela[i].pontos.toString()),
+          );
+        },
+        separatorBuilder: (_, __) => Divider(),
+        itemCount: controller.tabela.length,
+        padding: EdgeInsets.all(16),
       ),
     );
   }
